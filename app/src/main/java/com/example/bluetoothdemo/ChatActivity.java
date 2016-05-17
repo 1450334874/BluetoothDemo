9c46c32c77ad037ae1feb8635d620a424ca2770e
pdd <1450334874@qq.com>
2016-05-17 17:30:26 +0800

    聊天界面

--- app/src/main/java/com/example/bluetoothdemo/ChatActivity.java
+++ app/src/main/java/com/example/bluetoothdemo/ChatActivity.java
@@ -6,6 +6,7 @@ import android.os.PersistableBundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
+import android.view.MenuItem;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -26,5 +27,16 @@ public class ChatActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         setTitle(getIntent().getStringExtra("device_name"));
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:// 点击返回图标事件
+                this.finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
