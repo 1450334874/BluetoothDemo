537e6fce0c1f009158e6dddd53a0b70eb738cc82
pdd <1450334874@qq.com>
2016-05-17 17:17:27 +0800

    聊天界面

--- app/src/main/java/com/example/bluetoothdemo/ChatActivity.java
+++ app/src/main/java/com/example/bluetoothdemo/ChatActivity.java
@@ -4,11 +4,20 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.example.bluetoothdemo.adapter.ChatListAdapter;
 
 /**
  * Created by Administrator on 2016/5/16.
  */
 public class ChatActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private ChatListAdapter chatListAdapter;
+    private EditText message_et;
+    private Button send_btn;
 
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
